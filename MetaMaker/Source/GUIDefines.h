0b9fefd28111b68da184ebfbfc352d92ce327048
Strue Audio <strue-audio@Daniels-MacBook-Pro-2.local>
2019-09-02 11:00:00 +0200

    Created GuiDefines.

---
+++ MetaMaker/Source/GUIDefines.h
@@ -0,0 +1,21 @@
+/*
+  ==============================================================================
+
+    GUIDefines.h
+    Created: 2 Sep 2019 10:55:01am
+    Author:  Strue Audio
+
+  ==============================================================================
+*/
+
+#pragma once
+
+
+namespace GUIDefines {
+    static const int margin = 10;
+    static const int buttonHeight = 30;
+    static const int buttonAreaWidth = 150;
+    static const int initialFileBrowserWidth = 200; // Isn't that handled automatically when using a flexbox?
+    
+    
+}
