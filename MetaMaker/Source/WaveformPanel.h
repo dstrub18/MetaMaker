aab5db8cc189864a2ea1b8e96cccd3c390958934
Strue Audio <strue-audio@Daniels-MacBook-Pro-2.local>
2020-03-17 22:25:10 +0100

    Made loading bar for waveform panel and amplitudeZoomSlider.

--- MetaMaker/Source/WaveformPanel.h
+++ MetaMaker/Source/WaveformPanel.h
@@ -27,17 +27,45 @@ public:
     {
         // In your constructor, you should add any child components, and
         // initialise any special settings that your component needs.
+        
+        
         thumbnail.addChangeListener(this);
         
+        
         setSize(width, height);
+        
+        progress = thumbnail.getProportionComplete();
+        progressbar = std::make_unique<ProgressBar>(progress);
+        progressbar -> setColour(ProgressBar::backgroundColourId, Colours::aquamarine);
+        progressbar -> setColour(ProgressBar::foregroundColourId, Colours::green);
+        
+        progressbar -> setSize(200, 20);
+        progressbar -> setTopLeftPosition(0, 5);
+       
+        addChildComponent(*progressbar);
+        progressbar -> setVisible(false);
+        
+        amplitudeZoomSlider.setSliderStyle(Slider::SliderStyle::LinearVertical);
+        amplitudeZoomSlider.setTopLeftPosition(GUIDefines::universalWidth - 30, 0);
+        amplitudeZoomSlider.setVisible (false);
+        amplitudeZoomSlider.setSize(30, 200);
+        //amplitudeZoomSlider.setTextBoxStyle (Slider::TextBoxBelow, true, 400, 20);
+        amplitudeZoomSlider.setRange(0.1, 2.0);
+        amplitudeZoomSlider.setValue(1.0);
+        
+        amplitudeZoomSlider.setDoubleClickReturnValue(true, 1.0, ModifierKeys::noModifiers);
+        
+        
+        addAndMakeVisible (&amplitudeZoomSlider);
+        amplitudeZoomSlider.setVisible(false);
+        
+        //amplitudeZoomSlider.setNumDecimalPlacesToDisplay (10);
         
     }
 
     ~WaveformPanel()
     {
     }
-    
-    AudioThumbnail thumbnail;
     
     
     // ChangeListener override
@@ -46,6 +74,9 @@ public:
         if (source == &thumbnail) {
         }
     }
+    
+    AudioThumbnailCache thumbnailCache  {512};
+    AudioThumbnail thumbnail;
     
     
 //==================================================================================
@@ -60,19 +91,27 @@ public:
         */
 
         if (thumbnail.getNumChannels() != 0) {
-            repaint();
-            g.setColour(Colours::white);
+            
+            progress = thumbnail.getProportionComplete();
+            if (progress < 0.9)
+                progressbar -> setVisible (true);
+            else
+                progressbar -> setVisible (false);
+
+            repaint ();
+            g.setColour (Colours::white);
             g.fillRect (getLocalBounds());
             repaint ();
             g.setColour (Colours::red);
-            thumbnail.drawChannels(g, getLocalBounds(), 0.0, thumbnail.getTotalLength(), 1.0f);
+            thumbnail.drawChannels (g, getLocalBounds(), 0.0, thumbnail.getTotalLength(), amplitudeZoomSlider.getValue());
+            amplitudeZoomSlider.setVisible(true);
         }
         else
         {
             repaint ();
             g.fillAll (Colours::white);   // clear the background
-            
-            //g.drawRect (getLocalBounds(), 4);   // draw an outline around the component
+            amplitudeZoomSlider.setVisible(false);
+            g.drawRect (getLocalBounds(), 4);   // draw an outline around the component
 
         }
     }
@@ -85,8 +124,11 @@ public:
     }
 
 private:
+    double progress;
+    std::unique_ptr<ProgressBar> progressbar;
     
-    AudioThumbnailCache thumbnailCache  {512};
+    Slider amplitudeZoomSlider;
+    
     Rectangle<int> waveFormArea;
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (WaveformPanel)
